77ecc27f385e89c619b93e209a27a7ad94a878a0
Simon Wesp <s.wesp@gmx.net>
2026-04-24 12:27:06 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,15 @@
 import { fileURLToPath, URL } from 'node:url';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
-import type { UserConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 const nodeEnv = process.env.NODE_ENV ?? 'production';
 
-const getBaseConfig = (): UserConfig => ({
+const target = process.env.BUILD_TARGET || 'widget';
+
+// https://vite.dev/config/
+export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
@@ -17,22 +19,17 @@ const getBaseConfig = (): UserConfig => ({
   define: {
     'process.env.NODE_ENV': JSON.stringify(nodeEnv),
     'process.env': JSON.stringify({ NODE_ENV: nodeEnv }),
-    'process': JSON.stringify({ env: { NODE_ENV: nodeEnv } }),
+    process: JSON.stringify({ env: { NODE_ENV: nodeEnv } }),
     __VUE_PROD_DEVTOOLS__: true,
   },
-});
-
-const target = process.env.BUILD_TARGET || 'widget';
-
-// https://vite.dev/config/
-export default defineConfig({
-  ...getBaseConfig(),
   build: {
     lib: {
-      entry: fileURLToPath(new URL(target === 'widget' ? './src/widget.ts' : './src/cp.ts', import.meta.url)),
+      entry: fileURLToPath(
+        new URL(target === 'widget' ? './src/widget.ts' : './src/cp.ts', import.meta.url),
+      ),
       name: target === 'widget' ? 'UmamiIsApp' : 'UmamiIsCpApp',
       formats: ['iife'],
-      fileName: () => target === 'widget' ? 'widget.js' : 'cp.js',
+      fileName: () => (target === 'widget' ? 'widget.js' : 'cp.js'),
     },
     rollupOptions: {
       output: {
@@ -40,7 +37,10 @@ export default defineConfig({
         assetFileNames: '[name][extname]',
       },
     },
-    outDir: target === 'widget' ? '../src/assetbundles/craftumamiiswidget/dist' : '../src/assetbundles/craftumamiiscp/dist',
+    outDir:
+      target === 'widget'
+        ? '../src/assetbundles/craftumamiiswidget/dist'
+        : '../src/assetbundles/craftumamiiscp/dist',
     emptyOutDir: true,
     sourcemap: true,
     minify: false,
